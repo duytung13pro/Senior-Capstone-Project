a4a98715ad26d7d2f139aa0fdd1e2a2a0af17df3
nhck3001 <88605167+nhck3001@users.noreply.github.com>
2026-01-24 16:53:02 -0500

    Update model/User.java. Add getters and setters
    Alow the database to access the fields so that data can be written to the database

--- backend/src/main/java/com/main/backend/model/User.java
+++ backend/src/main/java/com/main/backend/model/User.java
@@ -13,5 +13,33 @@ public class User {
     private String email;
     private String password;
 
-    // getters & setters
+        // --- getters & setters ---
+        // We need getters so that MongoDB can access these fields and write them to the database
+        public String getId() {
+            return id;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public String getEmail() {
+            return email;
+        }
+    
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    
+        public String getPassword() {
+            return password;
+        }
+    
+        public void setPassword(String password) {
+            this.password = password;
+        }
 }
