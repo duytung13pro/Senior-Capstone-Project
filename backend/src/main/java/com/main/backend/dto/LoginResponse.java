f8c765002b54643a5f3de65aeda47809ba54ad35
nhck3001 <88605167+nhck3001@users.noreply.github.com>
2026-02-06 00:10:38 -0500

    Remove setters from dto for responses

--- backend/src/main/java/com/main/backend/dto/LoginResponse.java
+++ backend/src/main/java/com/main/backend/dto/LoginResponse.java
@@ -20,23 +20,12 @@ public class LoginResponse {
         return id;
     }
     
-    public void setId(String id) {
-        this.id = id;
-    }
-    
     public String getEmail() {
         return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
     }
     
     public Role getRole() {
         return role;
     }
-    
-    public void setRole(Role role) {
-        this.role = role;
-    }
+
 }
